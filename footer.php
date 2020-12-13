8395dcce5dc0a9618f94c6a763888fb307898015
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-12-13 18:27:41 -0500

    Start building template parts

--- footer.php
+++ footer.php
@@ -1,5 +1,20 @@
+<?php
+$footer_layout  = '3,3,3,3';
+$columns        = explode( ',', $footer_layout );
+$widgets_active = false;
+foreach ( $columns as $i => $column ) {
+	if ( is_active_sidebar( 'footer-sidebar-' . ( $i + 1 ) ) ) {
+		$widgets_active = true;
+	}
+}
+?>
 
+<?php if ( $widgets_active ) { ?>
+    <?php get_template_part('template-parts/footer/widgets'); ?>
+<?php } ?>
+
+<?php get_template_part( 'template-parts/footer/info' ); ?>
 
 <?php wp_footer(); ?>
-    </body>
+</body>
 </html>
